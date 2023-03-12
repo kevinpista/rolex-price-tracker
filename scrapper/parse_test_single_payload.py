80b49d999e643379e7033da1c57f897fad2bb33b
kevinpista <120353921+kevinpista@users.noreply.github.com>
2023-03-11 16:17:44 -0800

    Merge Watch Bulk Create API branch with scrapper

--- scrapper/parse_test_single_payload.py
+++ scrapper/parse_test_single_payload.py
@@ -2,7 +2,7 @@ import datetime
 import requests
 import json
 
-#date_today = datetime.date.today().isoformat() # YYYY-MM-DD date object format for our Watch Model
+date_today = datetime.date.today().isoformat() # YYYY-MM-DD date object format for our Watch Model
 
 #scrap_ref_nums = ['126200', '126300', '126234', '126334']
 
@@ -21,11 +21,11 @@ scrap_data1 = {
 'condition': 'New',
 'box_and_papers': True,
 'seller_location': 'United States',
-'date_scrapped': '2023-03-06'        
+'date_scrapped': f'{date_today}'        
 }
 
 scrap_data2 = {
-'ref_num': '126300',
+'ref_num': '126234',
 'price': 12000,
 'production_year': 2023,
 'condition': 'New',
@@ -35,14 +35,14 @@ scrap_data2 = {
 }
 scrap_data_list.append(scrap_data1)
 scrap_data_list.append(scrap_data2)
-print(scrap_data_list)
 
 
-endpoint_url = 'http://127.0.0.1:8000/watch/'
+endpoint_url = 'http://127.0.0.1:8000/watch/bulk-create/'
 headers = {'Content-Type': 'application/json'}
 response = requests.post(endpoint_url, data=json.dumps(scrap_data_list), headers=headers)
+message = response.json()['Message']
 
 if response.status_code == 200:
-    print("Success")
+    print(message)
 else:
-    print("Failed")
+    print(message)
