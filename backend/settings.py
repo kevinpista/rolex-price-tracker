fb61b2b2dc3a68d261d62ea62f7b68ab90f28050
kevinpista <120353921+kevinpista@users.noreply.github.com>
2023-07-16 16:07:23 -0700

    Deployment to Heroku

--- backend/settings.py
+++ backend/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases 
 
 
 # Password validation
