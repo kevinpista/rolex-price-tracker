c7aeba2dd479e678dab9ef86e95e7c3f8c56cc56
kevinpista <120353921+kevinpista@users.noreply.github.com>
2023-07-23 13:47:56 -0700

    CORS Whitelist update

--- backend/settings.py
+++ backend/settings.py
@@ -156,5 +156,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = [
-    'https://my-rolex-price-tracker-bb86e7034531.herokuapp.com', 'my-rolex-price-tracker-bb86e7034531.herokuapp.com', 
+    'https://my-rolex-price-tracker-bb86e7034531.herokuapp.com', 'my-rolex-price-tracker-bb86e7034531.herokuapp.com','www.my-rolex-price-tracker-bb86e7034531.herokuapp.com' 
 ]
